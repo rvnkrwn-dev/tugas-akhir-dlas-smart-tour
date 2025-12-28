c1b6f357c4296495a989c5ca53aaa74064f9f795
Revan Kurniawan <rvnkrwn@hotmail.com>
2025-12-28 20:32:51 +0000

    🚀 feat: add AuthDivider.vue

---
+++ app/components/Auth/AuthDivider.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="relative">
+    <div class="absolute inset-0 flex items-center">
+      <div class="w-full border-t border-gray-200"></div>
+    </div>
+    <div class="relative flex justify-center text-xs">
+      <span class="px-4 bg-white text-[#999]">
+        {{ text }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  text: 'or'
+})
+</script>
