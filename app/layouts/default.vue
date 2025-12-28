89c01e2310eb3dc5e9544ee65740792cd32467eb
Revan Kurniawan <rvnkrwn@hotmail.com>
2025-12-28 20:32:59 +0000

    🚀 feat: add default.vue

---
+++ app/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="min-h-screen bg-white">
+    <!-- Header/Navbar Component -->
+    <AppHeader />
+
+    <!-- Main Content -->
+    <main>
+      <slot />
+    </main>
+
+    <!-- Footer Component -->
+    <AppFooter />
+
+    <!-- Global Cart Components -->
+    <CartButton />
+    <CartDrawer />
+
+    <!-- Virtual Tour FAB Button -->
+    <VirtualTourFab />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Layout untuk halaman utama aplikasi
+</script>
